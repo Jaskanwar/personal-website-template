844c3cf92853e63b12969a8833cb067aade8415f
jaskanwar <jaskanwar.singh1@gmail.com>
2020-12-26 14:41:01 -0500

    Installed angular material & made about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoDisplayComponent } from './photo-display/photo-display.component';
 import { NavBarModule } from './nav-bar/nav-bar.module';
+import { AboutComponent } from './about/about.component';
 
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoDisplayComponent
+    PhotoDisplayComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavBarModule
+    NavBarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
