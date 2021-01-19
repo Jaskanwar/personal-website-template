446ddd985dfdb253abf8387a60a01312e1f4f646
jaskanwar <jaskanwar.singh1@gmail.com>
2021-01-19 15:18:12 -0500

    Fixed syntax error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import {MatGridListModule} from "@angular/material/grid-list";
     MatCardModule,
     MatButtonModule,
     ClipboardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
     MatGridListModule
   ],
   providers: [],
